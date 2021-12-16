b20b64d55bd6bf3c1039624072ef4c0182504d0a
ZoruaStar64 <sanderb119@gmail.com>
2021-12-16 11:49:41 +0100

    Level 1 is complete

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const Test = SpriteKind.create()
+    export const ReplacedTile = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (PlayerStages == 1) {
@@ -12,56 +13,70 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         CurrentPlayerAnim = 1
     }
 })
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    scene.setBackgroundColor(7)
-    PlayerStages = 1
-    animation.stopAnimation(animation.AnimationTypes.All, PlayerSprite)
-    PlayerSprite.setImage(assets.image`ClimatianFireFighter0`)
-    controller.moveSprite(PlayerSprite, 20, 20)
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (PlayerSprite.tileKindAt(TileDirection.Right, assets.tile`IslandPart1`) || (PlayerSprite.tileKindAt(TileDirection.Right, assets.tile`IslandPart5`) || (PlayerSprite.tileKindAt(TileDirection.Left, assets.tile`IslandPart4`) || PlayerSprite.tileKindAt(TileDirection.Left, assets.tile`IslandPart7`)))) {
+        TransportToLevel1()
+    } else if (PlayerSprite.tileKindAt(TileDirection.Bottom, assets.tile`IslandPart1`) || (PlayerSprite.tileKindAt(TileDirection.Top, assets.tile`IslandPart5`) || (PlayerSprite.tileKindAt(TileDirection.Bottom, assets.tile`IslandPart4`) || PlayerSprite.tileKindAt(TileDirection.Top, assets.tile`IslandPart7`)))) {
+        TransportToLevel1()
+    } else {
+    	
+    }
 })
+function DouseTreeFire () {
+    tiles.setTileAt(LocationOfFire, assets.tile`GrassTile`)
+    WaterBolt.destroy()
+    DousedFire = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.ReplacedTile)
+    tiles.placeOnTile(DousedFire, LocationOfFire)
+    animation.runImageAnimation(
+    DousedFire,
+    assets.animation`BurntTreeAnim`,
+    500,
+    true
+    )
+    info.changeScoreBy(250)
+    RemainingFires += -1
+    if (RemainingFires == 0) {
+        TransportToOcean()
+    }
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.stopAnimation(animation.AnimationTypes.All, PlayerSprite)
     if (CurrentPlayerAnim == 0 && PlayerStages == 1) {
         PlayerSprite.setImage(assets.image`FireFighterSpecialActionDown`)
-        WaterBolt = sprites.createProjectileFromSprite(assets.image`WaterBlastDown`, PlayerSprite, 0, 100)
+        WaterBolt = sprites.createProjectileFromSprite(assets.image`WaterBlastDown`, PlayerSprite, 0, 50)
+        WaterBolt.setBounceOnWall(false)
     } else if (CurrentPlayerAnim == 1) {
         PlayerSprite.setImage(assets.image`FireFighterSpecialActionUp`)
-        WaterBolt = sprites.createProjectileFromSprite(assets.image`WaterBlastUp`, PlayerSprite, 0, -100)
+        WaterBolt = sprites.createProjectileFromSprite(assets.image`WaterBlastUp`, PlayerSprite, 0, -50)
+        WaterBolt.setBounceOnWall(false)
     } else if (CurrentPlayerAnim == 2) {
         PlayerSprite.setImage(assets.image`FireFighterSpecialActionRight`)
-        WaterBolt = sprites.createProjectileFromSprite(assets.image`WaterBlastRight`, PlayerSprite, 100, 0)
+        WaterBolt = sprites.createProjectileFromSprite(assets.image`WaterBlastRight`, PlayerSprite, 50, 0)
+        WaterBolt.setBounceOnWall(false)
     } else if (CurrentPlayerAnim == 3) {
         PlayerSprite.setImage(assets.image`FireFighterSpecialActionLeft`)
-        WaterBolt = sprites.createProjectileFromSprite(assets.image`WaterBlastLeft`, PlayerSprite, -100, 0)
+        WaterBolt = sprites.createProjectileFromSprite(assets.image`WaterBlastLeft`, PlayerSprite, -50, 0)
+        WaterBolt.setBounceOnWall(false)
     } else {
         PlayerSprite.sayText("Illegal dousing attempt", 500, false)
     }
-})
-controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-    mySprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Enemy)
-    animation.runImageAnimation(
-    mySprite,
-    assets.animation`BurningTree`,
-    500,
-    false
-    )
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (PlayerStages == 0) {
@@ -99,6 +114,54 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         CurrentPlayerAnim = 2
     }
 })
+function TransportToOcean () {
+    tiles.setTilemap(tilemap`Ocean`)
+    scene.setBackgroundColor(9)
+    PlayerStages = 0
+    animation.stopAnimation(animation.AnimationTypes.All, PlayerSprite)
+    PlayerSprite.setImage(assets.image`ClimarineR`)
+    tiles.placeOnTile(PlayerSprite, tiles.getTileLocation(10, 8))
+    animation.stopAnimation(animation.AnimationTypes.All, DousedFire)
+    for (let value of sprites.allOfKind(SpriteKind.ReplacedTile)) {
+        value.destroy()
+    }
+    controller.moveSprite(PlayerSprite, 40, 40)
+}
+function DouseFire () {
+    tiles.setTileAt(LocationOfFire, assets.tile`GrassTile`)
+    tiles.setWallAt(LocationOfFire, false)
+    DousedFire = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.ReplacedTile)
+    tiles.placeOnTile(DousedFire, LocationOfFire)
+    animation.runImageAnimation(
+    DousedFire,
+    assets.animation`SmokeAnim`,
+    500,
+    true
+    )
+    WaterBolt.destroy()
+    info.changeScoreBy(100)
+    RemainingFires += -1
+    if (RemainingFires == 0) {
+        TransportToOcean()
+    }
+}
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (PlayerStages == 1) {
         animation.runImageAnimation(
@@ -111,22 +174,42 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
-    scene.setBackgroundColor(9)
-    PlayerStages = 0
-    animation.stopAnimation(animation.AnimationTypes.All, PlayerSprite)
-    PlayerSprite.setImage(assets.image`ClimarineR`)
-    controller.moveSprite(PlayerSprite, 20, 20)
+    TransportToOcean()
 })
-let mySprite: Sprite = null
+function TransportToLevel1 () {
+    tiles.setTilemap(tilemap`FireLevel1`)
+    PlayerStages = 1
+    animation.stopAnimation(animation.AnimationTypes.All, PlayerSprite)
+    PlayerSprite.setImage(assets.image`ClimatianFireFighter0`)
+    tiles.placeOnTile(PlayerSprite, tiles.getTileLocation(5, 15))
+    RemainingFires = 56
+    controller.moveSprite(PlayerSprite, 40, 40)
+}
+scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
+    if (tiles.tileAtLocationEquals(location, assets.tile`BurningTreeTile`)) {
+        LocationOfFire = location
+        DouseTreeFire()
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`FireTile`, function (sprite, location) {
+    tiles.placeOnTile(PlayerSprite, tiles.getTileLocation(5, 15))
+})
+scene.onOverlapTile(SpriteKind.Projectile, assets.tile`FireTile`, function (sprite, location) {
+    LocationOfFire = location
+    DouseFire()
+})
+let RemainingFires = 0
+let DousedFire: Sprite = null
 let WaterBolt: Sprite = null
+let LocationOfFire: tiles.Location = null
 let CurrentPlayerAnim = 0
 let PlayerStages = 0
 let PlayerSprite: Sprite = null
 tiles.setTilemap(tilemap`Ocean`)
 scene.setBackgroundColor(9)
 PlayerSprite = sprites.create(assets.image`ClimarineR`, SpriteKind.Player)
-controller.moveSprite(PlayerSprite, 20, 20)
+controller.moveSprite(PlayerSprite, 40, 40)
 tiles.placeOnTile(PlayerSprite, tiles.getTileLocation(10, 8))
-PlayerSprite.setStayInScreen(false)
 scene.cameraFollowSprite(PlayerSprite)
 PlayerStages = 0
+info.setScore(0)
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -124,6 +124,38 @@ namespace myTiles {
     export const tile60 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile61 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile64 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile65 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile66 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile67 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile68 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile70 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile69 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile71 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile73 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile72 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile74 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile76 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile77 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile75 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile63 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile62 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -145,7 +177,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [], -Infinity);
+`, [myTiles.transparency16], TileScale.Sixteen);
             case "Ocean":
             case "level2":return tiles.createTilemap(hex`1000100001010101010101010101010116160101010101161601010101010113020514010101130205140101010101130417010101011306081401010101011303140101010101151501010101010101150101010101010101011801010101010101010101010101011b1114010101010101010101010101130708140101010101010101010101011c12170101010101010101010101010101190101010101010101010101010101010101010101011801010101011618181601010101011b101a010101130b090a0c14010101130e0d0f140101011519151901010101011919190101010101010101010101010101010101010101010101010101010101010101010101`, img`
 . . . . . . . . . . . . . . . . 
@@ -165,6 +197,44 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile15,myTiles.tile16,myTiles.tile17,myTiles.tile18,myTiles.tile20,myTiles.tile21,myTiles.tile22,myTiles.tile23,myTiles.tile24,myTiles.tile25,myTiles.tile26,myTiles.tile27,myTiles.tile28,myTiles.tile29], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "FireLevel1":
+            case "FireLevel1":return tiles.createTilemap(hex`100010000f010110060f0701100f070f1001100f100110090c0c0c09090c070f0101010f010110090c0c0c09090c070101101001101001070a0a0a06010a0b05050203050f1001070a0a0a06010d0909090c0c010b08080b0204030b08080b080b0a0a01070f100110101001100f060f060a0a01071001010101010101100601060a0a01071001010101010101100601060a0a01070f100110101001100f0610060a0a010b0505050204030505050b04060a0a01010f01060a0a0e090909090d090e0e01011001060a0a0a1001010101010110010504050b0a0a0a010f10011001100f01100d0d090a0a0a0110010101100110010f1001010a0a0a01010101100f010101`, img`
+2 . . . 2 2 2 . . 2 2 2 . . . 2 
+. . . . . . . . . . 2 2 . . . 2 
+. . . . . . . . . . 2 . . . . . 
+. . . 2 . . . 2 . . 2 2 2 . . 2 
+2 . . 2 . . . 2 . . . . . . . . 
+2 2 2 2 . . . 2 2 2 2 2 2 . . . 
+2 2 . . . . . . . 2 2 2 2 . . . 
+2 . . . . . . . . . 2 . 2 . . . 
+2 . . . . . . . . . 2 . 2 . . . 
+2 2 . . . . . . . 2 2 . 2 . . . 
+2 2 2 2 . . . 2 2 2 2 . 2 . . . 
+. 2 . 2 . . . . . . . . . . . . 
+. . . 2 . . . . . . . . . . . . 
+2 . 2 2 . . . . 2 . . . . . 2 . 
+. . . . . . . . . . . . . . . . 
+2 . . . . . . . . . . . 2 . . . 
+`, [myTiles.transparency16,myTiles.tile64,myTiles.tile65,myTiles.tile66,myTiles.tile67,myTiles.tile68,myTiles.tile70,myTiles.tile69,myTiles.tile71,myTiles.tile73,myTiles.tile72,myTiles.tile74,myTiles.tile76,myTiles.tile77,myTiles.tile75,myTiles.tile63,myTiles.tile62], TileScale.Sixteen);
         }
         return null;
     })
@@ -262,6 +332,38 @@ namespace myTiles {
             case "tile59":return tile59;
             case "tile60":return tile60;
             case "tile61":return tile61;
+            case "GrassTile":
+            case "tile64":return tile64;
+            case "StairLeftTile":
+            case "tile65":return tile65;
+            case "StairRightTile":
+            case "tile66":return tile66;
+            case "StairMiddleTile":
+            case "tile67":return tile67;
+            case "CliffTile":
+            case "tile68":return tile68;
+            case "CliffTile1":
+            case "tile70":return tile70;
+            case "CliffTile0":
+            case "tile69":return tile69;
+            case "CliffTile2":
+            case "tile71":return tile71;
+            case "RoadSideways":
+            case "tile73":return tile73;
+            case "RoadVertical":
+            case "tile72":return tile72;
+            case "CliffCorner":
+            case "tile74":return tile74;
+            case "RoadCorner1":
+            case "tile76":return tile76;
+            case "RoadCorner2":
+            case "tile77":return tile77;
+            case "RoadQuadWay":
+            case "tile75":return tile75;
+            case "BurningTreeTile":
+            case "tile63":return tile63;
+            case "FireTile":
+            case "tile62":return tile62;
         }
         return null;
     })
